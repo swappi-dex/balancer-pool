9a1395457546fd12d575cecfe41e0e79fc0032c3
lin.wang <lin.wang@confluxnetwork.org>
2023-05-12 14:30:29 +0800

    project init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+function App() {
+  const [c, setC] = useState(0)
+  return (
+    <>
+      <div>
+        hello cfx
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+module.exports = {
+    content: [
+        path.resolve(__dirname, './src/*.{js,ts,jsx,tsx}'),
+    ],
+};
