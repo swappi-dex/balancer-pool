5e6c53c307ba154952afe7c5c19ee6f4ff9fc9aa
lin.wang <lin.wang@confluxnetwork.org>
2023-05-15 16:33:19 +0800

    feat: add Header connect wallet button and network info

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,195 @@
+import { useState } from 'react';
 
 function Tag({ className, ...rest }: React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>) {
     return <div className={`inline-block px-[15px] py-[3px] text-sm leading-[17px] rounded-[32px] border border-[#fff] ${className}`} {...rest} />;
+}
+
+function Header() {
+    return (
+        <div className="h-20 flex flex-row items-center justify-between">
+            <div className="w-[290px] h-10 bg-[url('/logo.svg')]"></div>
+            <div className="flex flex-row">
+                <div className="px-4 flex flex-row items-center h-10 text-sm leading-none rounded-[40px] text-white border border-current">
+                    <div className="w-2 h-2 rounded-full border-[2px] border-[#009595]/20 bg-[#009595] bg-clip-content"></div>
+                    <div className="ml-1 w-6 h-6 bg-[url(/conflux-network-icon.svg)]"></div>
+                    <div className="ml-1 font-medium">Conflux eSpace</div>
+                </div>
+                {/* connect button */}
+                {/* <div className="ml-5 h-10 px-6 rounded-full bg-[#38A0DA] text-sm leading-10 text-white">Connect Wallet</div> */}
+                <div className="ml-5 px-4 flex flex-row items-center h-10 text-sm leading-none rounded-[40px] text-white border border-current">
+                    {/* user avatar */}
+                    <div
+                        style={{
+                            backgroundImage: 'url(/conflux-network-icon.svg)',
+                            backgroundSize: '100% 100%',
+                        }}
+                        className="w-6 h-6 rounded-full "
+                    ></div>
+                    <div className="ml-2 font-medium">0x4001...4e85</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function PoolInfoAndMyLocked() {
+    const [tabIndex, setTabIndex] = useState(0);
+    const switchTab = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        const tabIndex = parseInt((e.target as HTMLElement).getAttribute('data-tab-index') || '0');
+        setTabIndex(tabIndex);
+    };
+    return (
+        <div className="w-full h-full pt-5 pb-[34px] pl-9 pr-5 flex flex-col">
+            <div className="flex flex-row">
+                <div
+                    onClick={switchTab}
+                    data-tab-index="0"
+                    className={`py-1 px-4 flex items-center rounded-[28px] text-base leading-5 font-medium cursor-pointer ${
+                        tabIndex === 0 ? 'bg-[#FFE99B]' : ''
+                    }`}
+                >
+                    Pool Info
+                </div>
+                <div
+                    onClick={switchTab}
+                    data-tab-index="1"
+                    className={`py-1 px-4 flex items-center rounded-[28px] text-base leading-5 font-medium cursor-pointer ${
+                        tabIndex === 1 ? 'bg-[#FFE99B]' : ''
+                    }`}
+                >
+                    My Locked
+                </div>
+            </div>
+            <div className="mt-6 flex-1 overflow-hidden flex flex-row justify-stretch">
+                <div
+                    style={{
+                        transform: `translateX(-${100 * tabIndex}%)`,
+                    }}
+                    className="pr-2 w-full transition-all flex-shrink-0 overflow-y-scroll scroll-bar"
+                >
+                    <table className="text-left w-full h-full">
+                        <thead className="sticky top-0 bg-[#FFCB14]">
+                            <tr className="top-0 left-0 right-0 overflow-visible ">
+                                <th className="relative w-full p-0 leading-none font-medium">
+                                    <span className="bottom-0 w-full h-[26px] bg-[#FFCB14] pl-4 pb-[13px] text-base leading-5 inline-block">Amount</span>
+                                </th>
+                                <th className="relative p-0 leading-none font-medium whitespace-nowrap">
+                                    <span className="bottom-0 w-full h-[26px] bg-[#FFCB14] pb-[13px] text-base leading-5 inline-block">Unlock Time</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody className="">
+                            {Array(10)
+                                .fill('$')
+                                .map((_, index) => {
+                                    return (
+                                        <tr className="h-[48px] w-full border-b last:border-none border-current text-base leading-5" key={index}>
+                                            <td className="pl-4 w-full font-black">100,000.00 LP </td>
+                                            <td className=" font-medium whitespace-nowrap">2023/08/08 14:00:00</td>
+                                        </tr>
+                                    );
+                                })}
+                        </tbody>
+                    </table>
+                </div>
+                <div
+                    style={{
+                        transform: `translateX(-${100 * tabIndex}%)`,
+                    }}
+                    className="pr-2 w-full transition-all flex-shrink-0 overflow-y-scroll scroll-bar"
+                >
+                    <table className="text-left w-full h-full">
+                        <thead className="sticky top-0 bg-[#FFCB14]">
+                            <tr className="top-0 left-0 right-0 overflow-visible">
+                                {[
+                                    {
+                                        label: 'Token',
+                                        className: 'pl-4 w-[130px]',
+                                    },
+                                    {
+                                        label: 'Weight',
+                                        className: 'w-[112px]',
+                                    },
+                                    {
+                                        label: 'Balance',
+                                        className: 'w-[150px]',
+                                    },
+                                    {
+                                        label: 'Value',
+                                        className: 'w-[160px]',
+                                    },
+                                    {
+                                        label: 'Token %',
+                                    },
+                                ].map(({ label, className }, index) => {
+                                    return (
+                                        <th key={index} className="relative p-0 leading-none font-medium">
+                                            <span
+                                                className={`${
+                                                    className || ''
+                                                } w-full h-[26px] bg-[#FFCB14] pb-[13px] text-base leading-5 inline-block whitespace-nowrap`}
+                                            >
+                                                {label}
+                                            </span>
+                                        </th>
+                                    );
+                                })}
+                            </tr>
+                        </thead>
+                        <tbody className="">
+                            {Array(10)
+                                .fill('$')
+                                .map((_, index) => {
+                                    return (
+                                        <tr className="h-[48px] w-full border-b last:border-none border-current text-base leading-5 font-medium" key={index}>
+                                            <td className="pl-4 whitespace-nowrap">CFX</td>
+                                            <td className="whitespace-nowrap">98</td>
+                                            <td className="whitespace-nowrap">300.000.12</td>
+                                            <td className="whitespace-nowrap">300.000.12</td>
+                                            <td className="whitespace-nowrap">79.21%</td>
+                                        </tr>
+                                    );
+                                })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 function App() {
     return (
         <>
             <div className="min-h-full flex justify-center bg-black">
-                <div className="min-w-[1440px] w-[1440px] h-[900px] px-[22px] pb-[22px] flex flex-col justify-between bg-[url('/bg-app.png')] bg-cover">
-                    <div className="h-20 flex flex-row items-center">
-                        <div className="w-[290px] h-10 bg-[url('/logo.svg')]"></div>
-                    </div>
+                <div className="min-w-[1440px] w-[1440px] h-[900px] px-5 pb-[22px] flex flex-col justify-between bg-[url('/bg-app.png')] bg-cover">
+                    <Header />
                     <div className="-mx-[22px] h-20 bg-[url('/title.svg')]"></div>
                     <div className="w-full h-[452px] flex flex-row justify-stretch ">
-                        <div className="flex-1 flex flex-col justify-between p-5 pb-6 rounded-[32px] text-white bg-[#38A0DA]">
-                            <div className="flex flex-row items-stretch">
-                                <div className="relative p-2 w-28 rounded-[28px] bg-white/60">
-                                    <div className='w-15 h-15 bg-cover bg-[url("/cfx-logo.png")]'></div>
-                                    <div className='absolute left-11 top-2 w-15 h-15 bg-cover bg-[url("/etc-logo.png")]'></div>
-                                </div>
-                                <div className="ml-5 flex flex-col justify-between">
-                                    <div className=" font-black text-[32px] leading-[39px]">ETC-CFX Weighted Pool</div>
-                                    <div>
-                                        <Tag>98%&nbsp;ETC</Tag>
-                                        <Tag className="ml-2">2%&nbsp;CFX</Tag>
+                        <div className="flex-1 rounded-[32px] text-white bg-[#38A0DA]">
+                            <div className="flex flex-col justify-between w-full h-full p-5 pb-6 ">
+                                <div className="flex flex-row items-stretch">
+                                    <div className="relative p-2 w-28 rounded-[28px] bg-white/60">
+                                        <div className='w-15 h-15 bg-cover bg-[url("/cfx-logo.png")]'></div>
+                                        <div className='absolute left-11 top-2 w-15 h-15 bg-cover bg-[url("/etc-logo.png")]'></div>
+                                    </div>
+                                    <div className="ml-5 flex flex-col justify-between">
+                                        <div className=" font-black text-[32px] leading-[39px]">ETC-CFX Weighted Pool</div>
+                                        <div>
+                                            <Tag>98%&nbsp;ETC</Tag>
+                                            <Tag className="ml-2">2%&nbsp;CFX</Tag>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div>
-                                <div className="mt-5 font-medium text-base leading-5">APR</div>
-                                <div className="mt-2 text-[68px] leading-[83px] font-black">100.00%</div>
-                                <div className="mt-5 font-medium text-base leading-5">Liquidity</div>
-                                <div className="mt-2 text-[68px] leading-[83px] font-black">$100,000,000</div>
+                                <div>
+                                    <div className="mt-5 font-medium text-base leading-5">APR</div>
+                                    <div className="mt-2 text-[68px] leading-[83px] font-black">100.00%</div>
+                                    <div className="mt-5 font-medium text-base leading-5">Liquidity</div>
+                                    <div className="mt-2 text-[68px] leading-[83px] font-black">$100,000,000</div>
+                                </div>
                             </div>
                         </div>
-                        <div className="flex-1 flex flex-col justify-stretch">
+                        <div className="flex-1 overflow-hidden flex flex-col justify-stretch">
                             <div className="flex-1 flex flex-row justify-stretch">
                                 <div className="flex-1 p-5 flex flex-col rounded-[32px] text-[#000] bg-white">
                                     <div className="flex-1">
@@ -58,45 +213,7 @@ function App() {
                                 </div>
                             </div>
                             <div className="flex-1 overflow-hidden rounded-[32px] bg-[#FFCB14] text-black">
-                                <div className="w-full h-full pt-5 pb-[34px] pl-9 pr-5 flex flex-col">
-                                    <div className="flex flex-row">
-                                        <div className="py-1 px-4 flex items-center rounded-[28px] text-base leading-5 font-medium">Pool Info</div>
-                                        <div className="py-1 px-4 flex items-center rounded-[28px] bg-[#FFE99B] text-base leading-5 font-medium">My Locked</div>
-                                    </div>
-                                    <div className="mt-6 pr-2 flex-1 overflow-y-scroll scroll-bar">
-                                        <table className="text-left w-full h-full">
-                                            <thead className="sticky top-0 bg-[#FFCB14]">
-                                                <tr className="top-0 left-0 right-0 overflow-visible">
-                                                    <th className="relative w-full p-0 leading-none">
-                                                        <span className="bottom-0 w-full h-[26px] bg-[#FFCB14] pl-4 pb-[13px] text-base leading-5 inline-block">
-                                                            Amount
-                                                        </span>
-                                                    </th>
-                                                    <th className="relative p-0 leading-none whitespace-nowrap">
-                                                        <span className="bottom-0 w-full h-[26px] bg-[#FFCB14] pb-[13px] text-base leading-5 inline-block">
-                                                            Unlock Time
-                                                        </span>
-                                                    </th>
-                                                </tr>
-                                            </thead>
-                                            <tbody className="">
-                                                {Array(10)
-                                                    .fill('$')
-                                                    .map((_, index) => {
-                                                        return (
-                                                            <tr
-                                                                className="h-[48px] w-full border-b last:border-none border-current text-base leading-5"
-                                                                key={index}
-                                                            >
-                                                                <td className="pl-4 w-full font-black">100,000.00 LP </td>
-                                                                <td className=" font-medium whitespace-nowrap">2023/08/08 14:00:00</td>
-                                                            </tr>
-                                                        );
-                                                    })}
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                </div>
+                                <PoolInfoAndMyLocked />
                             </div>
                         </div>
                     </div>
