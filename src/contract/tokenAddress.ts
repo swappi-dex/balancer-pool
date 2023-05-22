5cb5d1259ecbb182a3032b97159eb07181749eb9
lin.wang <mag1c_shen_09@foxmail.com>
2023-05-22 12:05:21 +0800

    feat: add PPI config

--- src/contract/tokenAddress.ts
+++ src/contract/tokenAddress.ts
@@ -4,3 +4,4 @@ const isDEV = import.meta.env.MODE === 'development';
 export const ETCTokenAddress = isDEV ? '0x446467fc6d81f6b285960e430ee7207e6fac0633' : '0x446467fc6d81f6b285960e430ee7207e6fac0633';
 export const CFXTokenAddress = isDEV ? '0x2ed3dddae5b2f321af0806181fbfa6d049be47d8' : '0x2ed3dddae5b2f321af0806181fbfa6d049be47d8';
 export const FaucetUSDTAddress = isDEV ? '0x7d682e65efc5c13bf4e394b8f376c48e6bae0355' : '0x7d682e65efc5c13bf4e394b8f376c48e6bae0355';
+export const PPITokenAddress = isDEV ? '0x49916ba65d0048c4bbb0a786a527d98d10a1cd2d' : '0x49916ba65d0048c4bbb0a786a527d98d10a1cd2d';
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,15 @@ import { UserConfig, defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import abi from './vite-abi-loader';
 
+// 0x49916ba65d0048c4bbb0a786a527d98d10a1cd2d
+
 export default defineConfig(async () => {
     return {
         plugins: [
             react(),
             abi({
                 addressMap: {
+                    PPI: '0xf256ea0bcb5a3a3a2cef0af4d927b1900f9fc18b',
                     SwappiFarmWeighted: '0x13319d155e2d8f26f421214111b5592a86088878',
                     SwappiRouterWeighted: '0xfbf882a0faa57bf8ec1741b08742333ca633fa89',
                     BaseSwappiFactoryWeighted: '0x36b83e0d41d1dd9c73a006f0c1cbc1f096e69e34',
